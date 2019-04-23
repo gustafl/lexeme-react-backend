2babe62f200f20440dbdde13b2fdebc7300b3ede
Gustaf Liljegren <gustafl@bahnhof.se>
2019-04-23 22:41:10 +0200

    Added first language services

---
+++ language.js
@@ -0,0 +1,34 @@
+let fs = require('fs');
+let path = require('path');
+let _ = require('lodash');
+
+module.exports = function(app) {
+
+    // Get a list of available languages
+    app.get('/language', function (req, res) {
+        let directory = './languages';
+        let output = [];
+        fs.readdir(directory, function (err, files) {
+            files.forEach(function (file) {
+                let regex = /\.(\w+)\.json/i;
+                let match = file.match(regex);
+                if (match.length > 0) {
+                    output.push(match[1]);
+                }
+            });
+            res.json(output);
+        });
+    });
+
+    // Get grammar for the given language code
+    app.get('/language/:code', function (req, res) {
+        let code = req.params.code;
+        let file = './languages/lang.' + code + '.json';
+        fs.readFile(file, 'utf8', function (err, data) {
+            if (err) throw err;
+            let obj = JSON.parse(data);
+            res.json(obj);
+        });
+    });
+}
+
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 let express = require('express');
+let cors = require('cors');
+let bodyParser = require('body-parser');
 
 let app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello world!');
-});
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+let cats = require('./language.js')(app);
 
 let server = app.listen(3001, function () {
     console.log('Server running at http://127.0.0.1:3001');
